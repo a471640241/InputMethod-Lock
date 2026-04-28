150e12dc79077c85fc09cd58366fbeeed175c82f
huanghongda <471640241@qq.com>
2026-04-28 10:11:40 +0800

    Initial Commit

---
+++ InputMethodGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InputMethodGuard
+//
+//  Created by 黄宏达 on 2026/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InputMethodGuard/InputMethodGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputMethodGuardApp.swift
+//  InputMethodGuard
+//
+//  Created by 黄宏达 on 2026/4/28.
+//
+
+import SwiftUI
+
+@main
+struct InputMethodGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
